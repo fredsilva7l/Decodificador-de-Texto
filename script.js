3c051cdaf6e277a1cd22191865d92bfe49a0c8b3
Perkles <fredericojunior1995@gmail.com>
2022-02-12 22:13:47 -0300

    41de100

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var input = document.querySelector("#input-text");
 
 var botaoCripto = (document.querySelector("#button-cripto"));
-botaoCripto.onclick = (startCripto);
+botaoCripto.onclick = (verificaTexto);
 
 var botaoDeCripto = (document.querySelector("#button-descripto"));
 botaoDeCripto.onclick = (startDeCripto);
@@ -9,99 +9,125 @@ botaoDeCripto.onclick = (startDeCripto);
 var botaoCopia = (document.querySelector("#button-copy"));
 botaoCopia.onclick = (copia);
 
-function cripto() {
-  var textoCripto = input.value
-       .replace(/e/gi, "enter")
-       .replace(/i/gi, "imes")
-       .replace(/a/gi, "ai")
-       .replace(/o/gi, "ober")
-       .replace(/u/gi, "ufat");
-    var output = (document.querySelector("#output-text"));
-    output.value = (textoCripto);
-    input.focus();
-    return(textoCripto);
-}
-
-function deCripto() {
-   var textoDecripto = input.value
-       .replace(/enter/gi, "e")
-       .replace(/imes/gi, "i")
-       .replace(/ai/gi, "a")
-       .replace(/ober/gi, "o")
-       .replace(/ufat/gi, "u");
-   var output = (document.querySelector("#output-text"));
-   output.value = (textoDecripto);
-   return(textoDecripto);
-}
-
-function copia(){
-   var copia = (document.querySelector("#output-text"));
-   navigator.clipboard.writeText(copia.value);
-   input.focus();
-   limpa();
-}
-
-function limpa() {
-   var limpa = (document.querySelector("#input-text"));
-   limpa.value = ("");
-   limpa.focus();
-   var limpa = (document.querySelector("#output-text"));
-   limpa.value = ("");
-}
-
-function startCripto() {
-   if(input.value == "") {
-      limpa();
-   }else {
-      verificaNumero();
-   }
+function verificaTexto() {
+    if ((verificaNumero() * verificaMaiscula() * verificaCaractere()) != 0) {
+        cripto();
+        exibeErro();
+    } else {
+        limpaOutput();
+        exibeErro();
+    }
 }
 
 function startDeCripto() {
-   var textoStart = (document.querySelector("#input-text"));
-   var palavrasChaves = ['enter', 'imes', 'ai', 'ober', 'ufat'];
-   var textoValor = (textoStart.value);
-   var verifica = palavrasChaves.some(chaves => textoValor.includes(chaves));
-   if(input.value == "") {
-       
-       limpa();
-   }else if(verifica) {
-       deCripto(); 
-   }else {
-       alert("TEXTO NÃO CRIPTOGRAFADO!");
-       limpa();
-   }
-}
-
-function verificaNumero () {
-    var textoCripto = input.value;
-    var verificaNumero = textoCripto;
-    for(var auxNum = 0 ; auxNum < verificaNumero.length; auxNum++) {
-				if(verificaNumero[auxNum] == " ")
-					auxNum++
-        else if(!isNaN(verificaNumero[auxNum])) {
-            alert("Não são permitidos números no texto");
-						limpa()
-            return 0;
-        }
+    var textoStart = (document.querySelector("#input-text"));
+    var palavrasChaves = ['enter', 'imes', 'ai', 'ober', 'ufat'];
+    var textoValor = (textoStart.value);
+    var verifica = palavrasChaves.some(chaves => textoValor.includes(chaves));
+    if (input.value == "") {
+        limpaInput();
+    } else if (verifica) {
+        var erroDecript = document.querySelector("#erroDecripto");
+        erroDecript.classList.add("erro")
+        deCripto();
+        limpaInput();
+    } else {
+        var erroDecript = document.querySelector("#erroDecripto");
+        erroDecript.classList.remove("erro")
     }
-    verificaTexto();
 }
 
-function verificaTexto () {
-    var textoCripto = input.value;
-    var verificaText = textoCripto;
-    for(var auxText = 0 ; auxText < verificaText.length; auxText++) {
+function copia() {
+    var copia = (document.querySelector("#output-text"));
+    navigator.clipboard.writeText(copia.value);
+    input.focus();
+    limpaInput();
+    limpaOutput();
+}
+
+function cripto() {
+    var textoCripto = input.value
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+    var output = (document.querySelector("#output-text"));
+    output.value = (textoCripto);
+    input.focus();
+    return (textoCripto);
+}
+
+function deCripto() {
+    var textoDecripto = input.value
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+    var output = (document.querySelector("#output-text"));
+    output.value = (textoDecripto);
+    return (textoDecripto);
+}
+
+function limpaInput() {
+    var limpaInput = (document.querySelector("#input-text"));
+    limpaInput.value = ("");
+    limpaInput.focus();
+
+}
+
+function limpaOutput() {
+    var limpaOutput = (document.querySelector("#output-text"));
+    limpaOutput.value = ("");
+}
+
+function verificaNumero() {
+    var verificaNumero = input.value;
+    for (var auxNum = 0; auxNum < verificaNumero.length; auxNum++) {
+
+        if (verificaNumero[auxNum] == " ") auxNum++
+
+        else if (!isNaN(verificaNumero[auxNum])) return 0;
+    }
+    return 1;
+}
+
+function verificaMaiscula() {
+    var verificaText = input.value;
+    for (var auxText = 0; auxText < verificaText.length; auxText++) {
         var conversao = verificaText[auxText].charCodeAt(0);
-        if (conversao == 32){
-					auxText++;
-				}
-				else if(conversao > 122 || conversao < 97){
-            alert("Existem letras maisculas ou caractere especial no seu texto");
-						limpa();
-            return 0;
-        }
+
+        if (conversao == 32) auxText++;
+        
+        else if (conversao >= 65 && conversao <= 90) return 0;
     }
-    cripto();
+    return 1;
 }
 
+function verificaCaractere() {
+    var verificaText = input.value;
+    for (var auxText = 0; auxText < verificaText.length; auxText++) {
+        var conversao = verificaText[auxText].charCodeAt(0);
+        
+        if (conversao == 32 || (conversao >= 65 && conversao <= 90) || !isNaN(verificaText[auxText])) auxText++;
+        
+        else if (conversao > 122 || conversao < 97) return 0;
+    }
+    return 1;
+}
+
+function exibeErro() {
+    var erroNumero = document.querySelector("#erroNumero");
+    var erroMaiscula = document.querySelector("#erroMaiscula");
+    var erroCaractere = document.querySelector("#erroCaractere");
+
+    if (verificaNumero() == 0) erroNumero.classList.remove("erro");
+    else erroNumero.classList.add("erro");
+
+    if (verificaMaiscula() == 0) erroMaiscula.classList.remove("erro");
+    else erroMaiscula.classList.add("erro");
+
+    if (verificaCaractere() == 0) erroCaractere.classList.remove("erro");
+    else erroCaractere.classList.add("erro");
+}
